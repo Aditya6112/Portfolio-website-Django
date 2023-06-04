af5bfb4e65efd9aca9ebc5b56f7136bf1ab73b6d
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2023-06-04 20:19:04 +0530

    Delete mysite directory

--- mysite/home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def home(request):
-    return HttpResponse("This is a boy view created by Aditya.")
-def paths(request):
-    return HttpResponse("This is a path view created by Aditya.")
